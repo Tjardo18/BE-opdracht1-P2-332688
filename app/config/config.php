2a4732b1df82750d209f48160f86c0637a680c39
Tjardo Ihrig <tjardoihrig.zakelijk@gmail.com>
2023-10-24 11:55:59 +0200

    Vorige opdracht fork

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("URLROOT", "http://be-opdracht3-j2-p2.com");
+define("APPROOT", dirname(dirname(__FILE__)));
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'instructeursindienst');
