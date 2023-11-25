6bc0127eff178a5d43b1163569edf06a2c295839
Tjardo Ihrig <tjardoihrig.zakelijk@gmail.com>
2023-11-25 21:00:36 +0100

    final commit

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define("URLROOT", "http://be-opdracht3-j2-p2.com");
+define("URLROOT", "http://instructeurindienst.com");
 define("APPROOT", dirname(dirname(__FILE__)));
 
 define('DB_HOST', 'localhost');
